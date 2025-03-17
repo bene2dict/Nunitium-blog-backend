31dd01b302e4e9a8e9f0aa43a7b551466109ec11
bene2dict <jeffmainonline@gmail.com>
2025-03-17 10:03:35 +0000

    cors temporary fix

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ connectDB();
 
 // Configure CORS
 app.use(cors({
-  origin: ["http://localhost:5173", "https://nunitium-blog.vercel.app", "nunitium-blog.vercel.app"], // Allow requests from frontend
+  origin: ["http://localhost:5173", "https://nunitium-blog.vercel.app", "nunitium-blog.vercel.app", "https://nunitium-blog.vercel.app/login", 
+    "nunitium-blog.vercel.app/login"
+  ], // Allow requests from frontend
   credentials: true, // Enable credentials (if using cookies or auth headers)
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
